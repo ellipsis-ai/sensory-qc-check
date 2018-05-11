07633e90dab77ccba13205df1e84653f010b29c3
Andrew Catton <>
2018-05-11 15:12:16 +0000

    Collect a picture if the appearance is off, share it via Box

--- actions/run-appearance-followup/function.js
+++ actions/run-appearance-followup/function.js
@@ -1,6 +1,24 @@
-function(contextString, crop, result, details, ellipsis) {
-  const Context = require('context');
+function(contextString, crop, result, details, picture, ellipsis) {
+  const box = require('ellipsis-box');
+const Context = require('context');
 const context = new Context(JSON.parse(contextString));
-const newResult = Object.assign({}, result, { details: details });
-context.acceptAppearanceResult(crop, newResult, ellipsis);
+
+withMaybePictureUrl().then(maybeUrl => {
+  const newResult = Object.assign({}, result, { details: details, pictureUrl: maybeUrl });
+  context.acceptAppearanceResult(crop, newResult, ellipsis);
+});
+
+function withMaybePictureUrl() {
+  return new Promise((resolve, reject) => {
+    if (picture) {
+      picture.fetch().then(res => {
+        box.files(ellipsis).uploadWithTimestamp(res.filename, res.contentType, res.value).then(res => {
+          resolve(res.url);
+        });
+      });
+    } else {
+      resolve(undefined);
+    }
+  });
+}
 }
